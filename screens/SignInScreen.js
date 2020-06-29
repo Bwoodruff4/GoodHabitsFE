2afdf560c43d7a1080fc311d9a9c7fe879dd86e8
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-06-29 16:06:26 -0600

    Added user sign up and login functionality that communicates with backend server

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { StyleSheet, View, TextInput } from 'react-native';
 import {
     Content,
@@ -11,20 +11,48 @@ import {
     Icon,
   } from "native-base";
 
-export default function SplashScreen() {
+const loginURL = `http://10.0.2.2:3000/login`
+
+export default function SignInScreen() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleUsernameChange = (val) => {
+        setUsername(val)
+    }
+    const handlePasswordChange = (val) => {
+        setPassword(val)
+    }
+
+    const login = () => {
+        fetch(loginURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user: {
+                    username: username,
+                    password: password,
+                }
+            })
+        })
+        .then(response => response.json())
+        .then(response => console.log(response))
+    }
     return (
         
         <View style={styles.container}>
             <Form style={styles.loginContainer}>
                 <Item>
                     <Icon active name="person" />
-                    <Input placeholder="Username" />
+                    <Input placeholder="Username" onChangeText={(val) => handleUsernameChange(val)}/>
                 </Item>
                 <Item>
                     <Icon active name="lock" />
-                    <Input secureTextEntry placeholder="Password" />
+                    <Input secureTextEntry placeholder="Password" onChangeText={(val) => handlePasswordChange(val)}/>
                 </Item>
-                <Button block rounded style={styles.button} onPress={() => {navigation.navigate('SignInScreen')}}>
+                <Button block rounded style={styles.button} onPress={login}>
                     <Text style={{color:'white', fontWeight:'bold'}}>Sign In</Text>
                 </Button>
             </Form>
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, View, TextInput } from 'react-native';
 import {
     Content,
@@ -11,28 +11,66 @@ import {
     Icon,
   } from "native-base";
 
-export default function SplashScreen() {
+const userURL = `http://10.0.2.2:3000/users`
+
+export default function SignUpScreen({navigation}) {
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmed, setPasswordConfirmed] = useState('')
+
+    const handleEmailChange = (val) => {
+        setEmail(val)
+    }
+    const handleUsernameChange = (val) => {
+        setUsername(val)
+    }
+    const handlePasswordChange = (val) => {
+        setPassword(val)
+    }
+    const handlePasswordConfirmedChange = (val) => {
+        setPasswordConfirmed(val)
+    }
+    const handleSubmit = () => {
+        fetch(userURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                user: {
+                    username: username,
+                    password: password,
+                    email: email
+                }
+            })
+        })
+        .then(response => response.json())
+        .then(response => console.log(response))
+
+        navigation.navigate('SignInScreen')
+    }
+
     return (
-        
         <View style={styles.container}>
             <Form style={styles.loginContainer}>
                 <Item>
                     <Icon active name="mail" />
-                    <Input placeholder="Email" />
+                    <Input placeholder="Email" onChangeText={(val) => handleEmailChange(val)}/>
                 </Item>
                 <Item>
                     <Icon active name="person" />
-                    <Input placeholder="Username" />
+                    <Input placeholder="Username" onChangeText={(val) => handleUsernameChange(val)}/>
                 </Item>
                 <Item>
                     <Icon active name="lock" />
-                    <Input secureTextEntry placeholder="Password" />
+                    <Input secureTextEntry placeholder="Password" onChangeText={(val) => handlePasswordChange(val)}/>
                 </Item>
                 <Item>
                     <Icon active name="lock" />
-                    <Input secureTextEntry placeholder="Re-enter Password" />
+                    <Input secureTextEntry placeholder="Re-enter Password" onChangeText={(val) => handlePasswordConfirmedChange(val)}/>
                 </Item>
-                <Button block rounded style={styles.button} onPress={() => {navigation.navigate('SignInScreen')}}>
+                <Button block rounded style={styles.button} onPress={handleSubmit}>
                     <Text style={{color:'white', fontWeight:'bold'}}>Sign Up</Text>
                 </Button>
             </Form>
